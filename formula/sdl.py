b847f91514adc8eac584f7ae82ede176bd230b0b
Matt Fichman <matt.fichman@gmail.com>
2013-10-31 21:44:28 -0400

    Added SDL formula

---
+++ formula/sdl.py
@@ -0,0 +1,14 @@
+import winbrew
+
+class Sdl(winbrew.Formula):
+    url = 'http://libsdl.org/release/SDL2-2.0.1.zip'
+    homepage = ''
+    sha1 = ''
+    build_deps = ()
+    deps = ()
+
+    def install(self):
+        pass
+
+    def test(self):
+        pass
