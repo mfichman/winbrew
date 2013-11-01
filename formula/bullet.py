6f2c2dd25d5a9242cc5dda3b2386e4be586dea3e
Matt Fichman <matt.fichman@gmail.com>
2013-10-31 21:24:28 -0400

    Updated bullet formula

--- formula/bullet.py
+++ formula/bullet.py
@@ -9,8 +9,9 @@ class Bullet(winbrew.Formula):
 
     def install(self):
         self.cd('bullet-2.82-r2704')
-        self.cmake()
-        self.nmake()
+        self.cmake(winbrew.cmake_args+('-G "Visual Studio 10"',)
+        self.msbuild(winbrew.msbuild_args+('BULLET_PHYSICS.sln'))
+
 
     def test(self):
         pass
