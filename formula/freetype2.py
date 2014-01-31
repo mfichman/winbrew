51ddbc9152c5283b9d225f955f0ee27a968933d3
Matt Fichman <matt.fichman@gmail.com>
2014-01-30 19:10:49 -0500

    Fixed freetype formula name

--- formula/freetype2.py
+++ formula/freetype2.py
@@ -1,6 +1,6 @@
 import winbrew
 
-class FreeType2(winbrew.Formula):
+class Freetype2(winbrew.Formula):
     url = 'http://downloads.sourceforge.net/project/freetype/freetype2/2.5.2/freetype-2.5.2.tar.bz2'
     homepage = ''
     sha1 = ''
