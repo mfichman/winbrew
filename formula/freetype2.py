dc33d0f56e27e843cbec9370b14493ffcf3cb8a8
Matt Fichman <matt.fichman@gmail.com>
2014-01-30 19:10:03 -0500

    Added freetype2 formula

---
+++ formula/freetype2.py
@@ -0,0 +1,18 @@
+import winbrew
+
+class FreeType2(winbrew.Formula):
+    url = 'http://downloads.sourceforge.net/project/freetype/freetype2/2.5.2/freetype-2.5.2.tar.bz2'
+    homepage = ''
+    sha1 = ''
+    build_deps = ()
+    deps = ('')
+
+    def install(self):
+        #self.cd('Box2D_v2.2.1\\Build\\vs2010')
+        #self.msbuild(winbrew.msbuild_args+('Box2D.vcxproj',))
+        #self.libs('bin\\Release')
+        #self.includes('..\\..\\Box2D', dest='Box2D')
+        pass
+
+    def test(self):
+        pass
