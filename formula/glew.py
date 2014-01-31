26231c72ca2444ad4cc331eb23c0bc8fef225edb
Matt Fichman <matt.fichman@gmail.com>
2014-01-31 09:54:01 -0500

    Updated glew formula with workaround for VS2013

--- formula/glew.py
+++ formula/glew.py
@@ -8,6 +8,15 @@ class Glew(winbrew.Formula):
     deps = ()
 
     def install(self):
+        # Workaround for broken RC compiler in Visual Studio 2013.  See http://sourceforge.net/p/glew/bugs/201/
+        import re
+        fd = open('glew.rc')
+        text = re.sub('VALUE "Comments".*$', '', fd.read())
+        fd.close()
+        fd = open('glew.rc', 'w')
+        fd.write(text)
+        fd.close()
+
         self.msbuild(winbrew.msbuild_args+('build\\vc10\\glew_static.vcxproj',))
         self.libs('lib\\Release\\Win32')
         self.includes('include')
