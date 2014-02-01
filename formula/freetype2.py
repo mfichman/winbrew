f84af4348f7ad424c3c5ae1ac5b38831273f6146
Matt Fichman <matt.fichman@gmail.com>
2014-02-01 13:17:42 -0500

    Updated freetype

--- formula/freetype2.py
+++ formula/freetype2.py
@@ -8,10 +8,11 @@ class Freetype2(winbrew.Formula):
     deps = ()
 
     def install(self):
-        self.cmake(('-G', 'Visual Studio 12'))
-        self.msbuild(winbrew.msbuild_args+('freetype.sln',))
-        self.includes('include')
-        self.libs('Release')
+        #self.cmake(('-G', 'Visual Studio 12'))
+        #self.msbuild(winbrew.msbuild_args+('freetype.sln',))
+        #self.includes('include')
+        #self.libs('Release')
+        self.system('jam')
 
     def test(self):
         pass
