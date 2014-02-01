d3d937dc8574122793652db686ee4c51bdbfdc18
Matt Fichman <matt.fichman@gmail.com>
2014-02-01 11:18:11 -0500

    Updated freetype

--- formula/freetype2.py
+++ formula/freetype2.py
@@ -4,15 +4,14 @@ class Freetype2(winbrew.Formula):
     url = 'http://downloads.sourceforge.net/project/freetype/freetype2/2.5.2/freetype-2.5.2.tar.bz2'
     homepage = 'http://freetype.org'
     sha1 = ''
-    build_deps = ('jam',)
+    build_deps = ('cmake',)
     deps = ()
 
     def install(self):
-        self.system('jam -sJAM_TOOLSET=VISUALC -sFT2_BUILD_INCLUDE=freetype2')
-        #self.cmake(('-G', 'Visual Studio 12'))
-        #self.msbuild(winbrew.msbuild_args+('freetype.sln',))
-        #self.includes('include', 'freetype2')
-        #self.libs('Release')
+        self.cmake(('-G', 'Visual Studio 12'))
+        self.msbuild(winbrew.msbuild_args+('freetype.sln',))
+        self.includes('include')
+        self.libs('Release')
 
     def test(self):
         pass
