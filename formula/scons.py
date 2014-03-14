4536428956cd5459046365cc607a24899ee51460
Matt Fichman <matt.fichman@gmail.com>
2014-03-13 20:17:59 -0400

    Added scons

---
+++ formula/scons.py
@@ -0,0 +1,15 @@
+
+import winbrew
+
+class Scons(winbrew.Formula):
+    url = 'https://downloads.sourceforge.net/scons/scons-2.3.0.tar.gz'
+    homepage = 'http://www.scons.org'
+    sha1 = '728edf20047a9f8a537107dbff8d8f803fd2d5e3'
+    build_deps = ()
+    deps = ()
+
+    def install(self):
+        self.system('python setup.py install')
+
+    def test(self):
+        pass
