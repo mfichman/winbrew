f35c32123ce4f1b80e6654163075f40a2660b562
Matt Fichman <matt.fichman@gmail.com>
2014-09-11 22:36:17 -0400

    Bump SDL2 version

--- formula/sdl2.py
+++ formula/sdl2.py
@@ -3,7 +3,7 @@ import os
 import glob
 
 class Sdl2(winbrew.Formula):
-    url = 'http://libsdl.org/release/SDL2-2.0.1.zip'
+    url = 'http://libsdl.org/release/SDL2-2.0.3.zip'
     homepage = 'http://libsdl.org'
     sha1 = '9283f1ce25b8f3155b6960b214cb6a706c285e27'
     build_deps = ()
