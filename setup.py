4fd1eda57548e8f13ad28e20042a2cd4326c12c1
Matt Fichman <matt.fichman@gmail.com>
2014-11-24 10:26:03 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'winbrew',
-    version = '1.1.0',
+    version = '1.1.1',
     author = 'Matt Fichman',
     author_email = 'matt.fichman@gmail.com',
     description = ('Native package installer for Windows'),
