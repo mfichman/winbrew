3aa6c3d4c55a2371493f83b87abe8e87ed8781c9
Matt Fichman <matt.fichman@gmail.com>
2018-01-15 10:55:12 -0500

    v1.2.0

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ setup(
     version = '1.2.0',
     author = 'Matt Fichman',
     author_email = 'matt.fichman@gmail.com',
-    description = ('Native package installer for Windows, a la Homebrew'),
+    description = ['Native package installer for Windows, a la Homebrew'],
     license = 'MIT',
-    keywords = ('installer', 'windows', 'package'),
+    keywords = ['installer', 'windows', 'package'],
     url = 'http://github.com/mfichman/winbrew',
-    packages = ('winbrew',),
-    install_requires = (
+    packages = ['winbrew',],
+    install_requires = [
         'patch>=1.15,<2',
-    ),
+    ],
     entry_points = {
-        'console_scripts': (
+        'console_scripts': [
             'winbrew = winbrew.execute:main',
             'brew = winbrew.execute:main'
-        )
+        ]
     }
 )
